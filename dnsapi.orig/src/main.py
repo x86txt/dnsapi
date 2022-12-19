f8151ce3f8dd1614ab71fcfd2eca416ca8b8fc5c
Matthew Evans <matt@secunit.io>
2022-12-18 21:47:27 -0500

    fixed error, looking a returning html response

--- dnsapi.orig/src/main.py
+++ dnsapi.orig/src/main.py
@@ -1,14 +1,28 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
 import dns.resolver
 import dns.reversename
-import json
+
 
 app = FastAPI()
 
-@app.get("/api/")
-async def read_item(domain: str | None = None):
+@app.get("/api/", response_class=HTMLResponse)
+async def read_items(domain: str):
     a = dns.resolver.resolve(domain, 'A')
+
+    html_content = """
+        <html>
+            <head>
+                <title>Some HTML in here</title>
+            </head>
+            <body>
+                <h1>Look ma! HTML!</h1>
+            </body>
+        </html>
+        """
+    
     for val in a:
-        print(vars(a))
-        #return {"IP": val}
+        return HTMLResponse(content=html_content, status_code=200)
+        return {"The IP for " + domain + " is": val.to_text()}
 
